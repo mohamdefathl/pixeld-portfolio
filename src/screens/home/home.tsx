4c00e31d7ca134c01f8c04721849eb3c77f4fa7d
mohamdefathl <mohammadefathl@gmail.com>
2024-12-08 00:36:32 +0300

    refactor: remove unused code

--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { TypingAnimation } from '../../components/typing-text/typing-text';
-
+interface Section {
+  name: string;
+  route: string;
+}
 export const Home: React.FC = () => {
   const navigate = useNavigate();
-  const [sections] = useState<Array<{ name: string, route: string }>>([
+  const [sections] = useState<Section[]>([
     { name: 'Experience', route: 'experience' },
     { name: 'Learning & Training', route: 'learning-training' },
     { name: 'Certifications & Projects', route: 'certifications-projects' },
@@ -20,7 +23,7 @@ export const Home: React.FC = () => {
     <div className="home-container">
       <TypingAnimation duration={70} text='Hey, I am Mohammed Al-ghorbani, a fanatical software developer.' />      
       <div style={{ marginTop: '5rem' }}>
-        {sections.map((message, index) => (
+        {sections.map((message:Section, index:number) => (
           <q
             key={index}
             onClick={() => handleSectionClick(message.route)}

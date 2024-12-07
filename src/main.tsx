a2b50f8a3620861945ec0dff42dee1ae58650d86
mohamdefathl <mohammadefathl@gmail.com>
2024-12-07 20:18:12 +0300

    fix: fix url name

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename="/pixle-portfolio/">
+  <BrowserRouter basename="/pixeld-portfolio/">
     <App />
   </BrowserRouter>
 )
